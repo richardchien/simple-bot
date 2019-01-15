92846007a1fbef229515e559e8f4ff754fee7488
Richard Chien <richardchienthebest@gmail.com>
2019-01-15 15:11:37 +0800

    添加了知乎日报功能

--- plugins.py
+++ plugins.py
@@ -4,6 +4,8 @@
 
 import random
 
+import requests
+
 from command import on_command
 
 
@@ -27,3 +29,30 @@ def _(bot, ctx, arg):
     expression = arg.strip()
     print(expression)
     return {'reply': str(eval(expression))}
+
+
+@on_command('知乎日报')
+def _(bot, ctx, arg):
+    STORY_URL_FORMAT = 'https://daily.zhihu.com/story/{}'
+
+    resp = requests.get(
+        'https://news-at.zhihu.com/api/4/news/latest',
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        }
+    )
+
+    data = resp.json()
+    stories = data.get('stories')
+
+    if not stories:
+        bot.send(ctx, '暂时没有数据，或者服务无法访问')
+        return
+
+    reply = ''
+    for story in stories:
+        url = STORY_URL_FORMAT.format(story['id'])
+        title = story.get('title', '未知内容')
+        reply += f'\n{title}\n{url}\n'
+
+    bot.send(ctx, reply)
