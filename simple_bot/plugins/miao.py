62003b3f4497f850dc7e9dcaa6a56b8b073a2c52
Richard Chien <richardchienthebest@gmail.com>
2019-01-18 13:26:21 +0800

    Update

--- simple_bot/plugins/miao.py
+++ simple_bot/plugins/miao.py
@@ -1,6 +1,27 @@
-from nonebot import on_command, CommandSession
+from nonebot import on_command, CommandSession, on_request, RequestSession
+from nonebot import permission as perm
+from nonebot import CQHttpError
 
 
 @on_command('miao', aliases=['喵一个', '喵喵喵', '喵'])
 async def _(session: CommandSession):
     await session.send('喵～')
+
+
+@on_command('get_member_count', aliases=['总人数'], permission=perm.GROUP,
+            only_to_me=False)
+async def _(session: CommandSession):
+    try:
+        member_list = await session.bot.get_group_member_list(
+            group_id=session.ctx['group_id']
+        )
+    except CQHttpError:
+        await session.send('无法获取')
+        return
+
+    await session.send(f'群里一共有 {len(member_list)} 个人')
+
+
+@on_request('friend')
+async def _(session: RequestSession):
+    await session.approve()
--- simple_bot/plugins/weather.py
+++ simple_bot/plugins/weather.py
@@ -1,10 +1,12 @@
+import re
+
 from nonebot import on_command, CommandSession
 
 
 @on_command('weather', aliases=['天气', '查天气'])
 async def weather(session: CommandSession):
     city = session.get('city', prompt='你想查哪个城市呢？')
-    date = session.get('date', prompt='你想查哪一天的？')
+    date = session.get('date', prompt='你想查哪一天的（格式 20190116）？')
     await session.send('你查询的城市是' + city)
     await session.send('你想查询的日期是' + date)
 
@@ -15,4 +17,9 @@ async def _(session: CommandSession):
         return
 
     print(session.current_key)
+    if session.current_key == 'date':
+        # if not re.match(r'^\d{8}$', session.current_arg_text):
+        if not re.fullmatch(r'\d{8}', session.current_arg_text):
+            session.pause('日期格式错误，请重新输入')
+
     session.args[session.current_key] = session.current_arg_text
